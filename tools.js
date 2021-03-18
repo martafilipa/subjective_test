9e1ad94357102e7cd7c5938ef17e996cf23eb4d3
Marta Marques <marta.p.marques@tecnico.ulisboa.pt>
2021-03-18 18:19:56 +0000

    Fix get_order

--- tools.js
+++ tools.js
@@ -4,64 +4,48 @@ const gc = 9;
 const n_cross = n + 72;
 const gh = 3;
 const n_total = n_cross + 24;
+const max_it = 5000;
 
 module.exports = {
-
-    // get_order: function(){
-    //     const n = 168;
-    //     const n_total = n + 120; 
-    //     const g = 21;
-    //     var items   = Array.from({length: n}, (x, i) => i);
-    //     var j = -1;
-    //     var res = [];
-    //     for(var i=0; i<n; i++){
-    //         if(j==-1){
-    //             x = randomInteger(0, items.length-1);
-    //             res[i] = items[x];
-    //             j = 0;
-    //         }
-    //         else{
-    //             do{
-    //                 x = randomInteger(0, items.length-1);
-    //                 res[i] = items[x];
-    //                 // console.log('Checking:');
-    //                 // console.log(res[i], res[i-1]);
-    //                 // console.log(res[i]%g, res[i-1]%g )z
-    //             }while(Math.floor(res[i]/g) == Math.floor(res[i-1]/g));
-    //         }
-    //         items.splice(x, 1);
-    //     }
-    //     return res;
-    // }
-
     get_order: function(){
-        var items   = Array.from({length: n_total}, (x, i) => i);
-        var last = [];
-        var res = [];
-        for(var i=0; i<n_total; i++){
-            if(last.length == 0){
-                x = randomInteger(0, items.length-1);
-                res[i] = items[x];
-                last.push(res[i]);
-            }
-            else{
-                do{
-                    x = randomInteger(0, items.length-1);
-                    res[i] = items[x];
-                }while(last.some(j => getIndex(res[i]).includes(j)));
-            }
-            items.splice(x, 1);
-            last = getIndex(res[i]);
+
+        var res = try_get_order();
+        while(res.length==0){
+            res = try_get_order()
         }
         return res;
     }
 }
-
+function try_get_order(){
+    var items   = Array.from({length: n_total}, (_, i) => i);
+    var last = [];
+    var res = [];
+    var k = 0;
+    for(var i=0; i<n_total; i++){
+        if(last.length == 0){
+            x = randomInteger(0, items.length-1);
+            res[i] = items[x];
+            last.push(res[i]);
+        }
+        else{
+            do{
+                x = randomInteger(0, items.length-1);
+                res[i] = items[x];
+                k++;
+            }while(last.some(j => getIndex(res[i]).includes(j)) && k < max_it);
+        }
+        if(k>max_it)
+            return [];
+        items.splice(x, 1);
+        last = getIndex(res[i]);
+    }
+    return res;
+}
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getIndex(x) {
+function getIndex(x){
     var cross = {
         0: [0, 1],
         1: [1, 2], 
